165d8f941f5b1a463533e0c6b10ffdbab1b5b09e
retr0 <jgr.espiga@protonmail.com>
2025-07-11 17:01:50 +0200

    Refactor `main.rs` variable names

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,13 @@ pub mod parser;
 use self::server::{Server, Config};
 
 fn main() {
-    let ip = std::net::IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1));
+    let host = std::net::IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1));
+    let port = 8080;
 
-    let conf = Config { host: ip, port: 8080, };
+    let conf = Config { host, port, };
 
     let server = Server::new(conf);
 
-    // match server.run() {
-    //     Ok(_) => println!("Server listening on port {}", server.config.port),
-    //     Err(e) => println!("Error {}", e),
-    // };
     server.run().unwrap();
 }
 
